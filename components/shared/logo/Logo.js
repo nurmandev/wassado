22131818a311f4eb7a375d7cdbbcac86fbd2435f
Adebayour66265 <adebayour66265@gmail.com>
2024-10-12 09:14:06 +0100

    update company logo

--- components/shared/logo/Logo.js
+++ components/shared/logo/Logo.js
@@ -1,80 +1,65 @@
-// /**
-//  * Title: Write a program using JavaScript on Logo
-//  * Author: Hasibul Islam
-//  * Portfolio: https://devhasibulislam.vercel.app
-//  * Linkedin: https://linkedin.com/in/devhasibulislam
-//  * GitHub: https://github.com/in/devhasibulislam
-//  * Facebook: https://facebook.com/in/devhasibulislam
-//  * Instagram: https://instagram.com/in/devhasibulislam
-//  * Twitter: https://twitter.com/in/devhasibulislam
-//  * Pinterest: https://pinterest.com/in/devhasibulislam
-//  * WhatsApp: https://wa.me/8801906315901
-//  * Telegram: devhasibulislam
-//  * Date: 15, August 2023
-//  */
-
-// // import Image from "next/image";
-// import { useRouter } from "next/router";
-// import React from "react";
-// import LoadImage from "../image/LoadImage";
-
-// const Logo = () => {
-//   const router = useRouter();
-
-//   // function toBase64(str) {
-//   //   return btoa(unescape(encodeURIComponent(str)));
-//   // }
-
-//   // function shimmer(width, height) {
-//   //   return `https://placehold.co/${width}x${height}.svg`;
-//   // }
-
-//   return (
-//     <>
-//       <LoadImage
-//         src="/assets/logo.svg"
-//         alt="logo"
-//         // placeholder="blur"
-//         // blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(60, 50))}`}
-//         /**
-//          * 1st Parameter: Width
-//          * 2nd Parameter: Height
-//          */
-//         title="logo"
-//         width={60}
-//         height={50}
-//         style={{
-//           maxWidth: "100%",
-//           height: "auto",
-//         }}
-//         onClick={() => router.push("/")}
-//         className="cursor-pointer object-center max-w-full"
-//       />
-//     </>
-//   );
-// };
-
-// export default Logo;
+// import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
+import LoadImage from "../image/LoadImage";
 
 const Logo = () => {
   const router = useRouter();
 
+  // function toBase64(str) {
+  //   return btoa(unescape(encodeURIComponent(str)));
+  // }
+
+  // function shimmer(width, height) {
+  //   return `https://placehold.co/${width}x${height}.svg`;
+  // }
+
   return (
-    <div
-      onClick={() => router.push("/")}
-      className="cursor-pointer text-center"
-      style={{
-        fontSize: "24px",
-        fontWeight: "bold",
-        fontFamily: "Edu AU VIC WA NT Guides', sans-serif", // Apply the imported font
-        color: "#333", // Customize the color as needed
-      }}
-    >
-      wassado
-    </div>
+    <>
+      <LoadImage
+        src="/assets/logo.png"
+        alt="logo"
+        // placeholder="blur"
+        // blurDataURL={`data:image/svg+xml;base64,${toBase64(shimmer(60, 50))}`}
+        /**
+         * 1st Parameter: Width
+         * 2nd Parameter: Height
+         */
+        title="logo"
+        width={60}
+        height={50}
+        style={{
+          maxWidth: "100%",
+          height: "auto",
+        }}
+        onClick={() => router.push("/")}
+        className="cursor-pointer object-center max-w-full"
+      />
+    </>
   );
 };
 
 export default Logo;
+// import { useRouter } from "next/router";
+// import React from "react";
+
+// const Logo = () => {
+//   const router = useRouter();
+
+//   return (
+//     <div
+//       onClick={() => router.push("/")}
+//       className="cursor-pointer text-center"
+//       style={{
+//         fontSize: "24px",
+//         fontWeight: "bold",
+//         fontFamily: "Edu AU VIC WA NT Guides', sans-serif", // Apply the imported font
+//         color: "#333", // Customize the color as needed
+//       }}
+//     >
+//       wassado
+//     </div>
+//   );
+// };
+
+// export default Logo;
