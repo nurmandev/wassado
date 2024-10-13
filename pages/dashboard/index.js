c126e9031e4b12392bca6b7af61fdcb8a6c9536e
Adebayour66265 <adebayour66265@gmail.com>
2024-10-13 21:39:05 +0100

    add dashbord

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,11 +1,12 @@
 import Panel from "@/layouts/Panel";
 import React from "react";
+import { DashboardUi } from "./dashbord_comp";
 
 const Dashboard = () => {
   return (
     <Panel>
       <div className="w-full h-full flex justify-center items-center !rounded">
-       Hello
+       <DashboardUi/>
       </div>
     </Panel>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {
-      borderRadius: {
-        DEFAULT: "10px",
-        primary: "1.5rem",
-        secondary: "9999px",
-      },
-      padding: {
-        primary: "1rem",
-        secondary: "1.5rem",
-      },
-      colors: {
-        primary: "#01BC1FD6",
-        secondary: "#E4FFEA",
-        neutral: "#515151",
-      },
-    },
+  	extend: {
+  		borderRadius: {
+  			DEFAULT: '10px',
+  			primary: '1.5rem',
+  			secondary: '9999px',
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		padding: {
+  			primary: '1rem',
+  			secondary: '1.5rem'
+  		},
+  		colors: {
+  			primary: '#01BC1FD6',
+  			secondary: '#E4FFEA',
+  			neutral: '#515151'
+  		}
+  	}
   },
-  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar-hide")],
+  plugins: [require("@tailwindcss/forms"), require("tailwind-scrollbar-hide"), require("tailwindcss-animate")],
 };
