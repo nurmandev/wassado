d15e912a177079ced2e1e1ea71be1843d99626cf
nurmandev <adebayour66265@gmail.com>
2024-10-18 22:45:08 +0100

    remove home dashbord

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,12 +1,13 @@
 import Panel from "@/layouts/Panel";
 import React from "react";
-import { DashboardUi } from "./dashbord_comp";
+// import { DashboardUi } from "./dashbord_comp";
 
 const Dashboard = () => {
   return (
     <Panel>
       <div className="w-full h-full flex justify-center items-center !rounded">
-       <DashboardUi/>
+        {/* <DashboardUi/> */}
+        Welcome to Your dashboard
       </div>
     </Panel>
   );
